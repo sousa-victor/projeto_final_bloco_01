05bce779d166780d58bb46aa3fca1223d5b4503a
sousa-victor <90732816+sousa-victor@users.noreply.github.com>
2023-09-11 20:04:56 -0300

    Controller

---
+++ projeto_final_bloco_01/Controller/AnuncioController.cs
@@ -0,0 +1,108 @@
+﻿using projeto_final_bloco_01.Model;
+using projeto_final_bloco_01.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_final_bloco_01.Controller
+{
+    public class AnuncioController : IRepository
+    {
+        private readonly List<Principal> listaAnuncios = new();
+        int numero = 0;
+
+
+        public void AddAnuncio(Estendida anuncio)
+        {
+            listaAnuncios.Add(anuncio);
+            Console.WriteLine("O anúncio número: " + anuncio.GetIdAnuncio() + " foi criada");
+        }
+
+        public void Atualizar(Estendida anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idAnuncio)
+        {
+            var anuncio = BuscaNaCollection(idAnuncio);
+
+            if (anuncio != null)
+            {
+                if (listaAnuncios.Remove(anuncio) == true)
+                {
+                    Console.WriteLine("O anúncio ID " + idAnuncio + " foi apagado com sucesso");
+                }
+                else
+                {
+                    Console.WriteLine("Anúncio não encontrado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Anúncio não encontrado");
+            }
+        }
+
+        public void ListarTudo()
+        {
+            foreach (var principal  in listaAnuncios)
+            {
+                principal.Visualizar();
+            }
+        }
+
+        public void ProcuraId(int idAnuncio)
+        {
+            var anuncio = BuscaNaCollection(idAnuncio);
+
+            if (anuncio != null)
+            {
+                anuncio.Visualizar();
+            }
+            else
+            {
+                Console.WriteLine("Anúncio não encontrado");
+            }
+        }
+
+        //Métodos auxiliares
+        public int GerarNumero()
+        {
+            return ++numero;
+        }
+        /*
+        void IRepository.ProcuraId(int idAnuncio)
+        {
+            var anuncio = BuscaNaCollection(idAnuncio);
+
+                if (anuncio != null)
+            {
+                anuncio.Visualizar();
+            }
+            else
+            {
+                Console.WriteLine("Anúncio não encontrado");
+            }
+        }*/
+
+        public Principal? BuscaNaCollection(int idAnuncio)
+        {
+            foreach (var principal in listaAnuncios)
+            {
+                if (principal.GetIdAnuncio() == idAnuncio)
+                {
+                    return principal;
+                }
+            }
+            return null;
+        }
+
+        public void Deletar(Estendida anuncio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- projeto_final_bloco_01/Program.cs
+++ projeto_final_bloco_01/Program.cs
@@ -1,4 +1,5 @@
-﻿using projeto_final_bloco_01.Model;
+﻿using projeto_final_bloco_01.Controller;
+using projeto_final_bloco_01.Model;
 
 namespace projeto_final_bloco_01
     
@@ -7,13 +8,15 @@ namespace projeto_final_bloco_01
     {
         static void Main(string[] args)
         {
-            int opcao = 5;
+            int opcao = 6;
 
             int idAnuncio, ano, numLugares;
-            string marca, modelo, categoria;
+            string marca, modelo, categoria, entrada;
+            bool exotico;
 
-            Estendida a1 = new Estendida(1, 2014, 2, "Audi", "R8", "esportivo", true);
-            a1.Visualizar();
+            AnuncioController anuncio = new();
+
+
 
             do
             {
@@ -30,7 +33,8 @@ namespace projeto_final_bloco_01
                 Console.WriteLine("*                   2 - Consultar carros cadastrados           *");
                 Console.WriteLine("*                   3 - Editar informações                     *");
                 Console.WriteLine("*                   4 - Excluír carros do banco de dados.      *");
-                Console.WriteLine("*                   5 - Sair                                   *");
+                Console.WriteLine("*                   5 - Pesquisar por ID do anúncio            *");
+                Console.WriteLine("*                   6 - Sair                                   *");
                 Console.WriteLine("*                                                              *");
                 Console.WriteLine("****************************************************************");
                 Console.ResetColor();
@@ -50,22 +54,34 @@ namespace projeto_final_bloco_01
                         Console.WriteLine("Você selecionou: Cadastro de novo veículo!");
                         Console.WriteLine("Por favor informe os seguintes dados:");
 
-                        Console.Write("ID do anúncio: ");
-                        idAnuncio = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Marca: ");
                         marca = Console.ReadLine();
                         Console.Write("Modelo: ");
                         modelo = Console.ReadLine();
                         Console.Write("Ano: ");
                         ano = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Categoria: (SUV/Hatch/Sedan/Esportivo");
+                        Console.Write("Categoria: (SUV/Hatch/Sedan/Esportivo)\n");
                         categoria = Console.ReadLine();
                         Console.Write("Número de lugares: ");
                         numLugares = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("É um carro exótico? S/N");
+                        entrada = Console.ReadLine();
+                        entrada.ToUpper();
+                        if (entrada == "S")
+                        {
+                            exotico = true;
+                        }
+                        else
+                        {
+                            exotico = false;
+                        }
+
+                        anuncio.AddAnuncio(new Estendida(anuncio.GerarNumero(), ano, numLugares, marca, modelo, categoria, exotico));
                         break;
 
                     case 2:
-                        Console.WriteLine("Informe o ID do anúncio");
+                        Console.WriteLine("Carros cadastrados: ");
+                        anuncio.ListarTudo();
                         break;
 
                     case 3:
@@ -73,10 +89,18 @@ namespace projeto_final_bloco_01
                         break;
 
                     case 4:
-                        Console.WriteLine("Excluir anúncio");
+                        Console.WriteLine("Digite o ID do anúncio");
+                        idAnuncio = Convert.ToInt32(Console.ReadLine());
+                        anuncio.Deletar(idAnuncio);
                         break;
 
                     case 5:
+                        Console.WriteLine("Digite o ID");
+                        idAnuncio = Convert.ToInt32(Console.ReadLine());
+                        anuncio.ProcuraId(idAnuncio);
+                        break;
+
+                    case 6:
                         Console.WriteLine("Programa encerrado");
                         break;
 
